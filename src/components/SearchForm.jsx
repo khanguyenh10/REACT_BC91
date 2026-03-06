d7cec272f3c3c3ad6caa5d8f61d8344509283012
khanguyenh10dev <khanguyenh10dev@gmail.com>
2026-03-06 14:43:50 +0700

    Update

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SearchForm = () => {
     return (
         <div className='search-form'>
-            <form className="form">
+            <form className="form form-search">
                 <div className="py-4 p-md-5 d-flex gap-2 gap-md-5 align-items-end">
                     <div className="form-item ">
                         <label >Search</label> <br />
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -23,8 +23,16 @@ const Login = () => {
                         <div className="col-md-6 col-lg-4 mb-3">
                             <div className=' d-flex gap-2 justify-content-end align-items-center'>
                                 <Link to={'/register'} className=' text-primary '>Register?</Link>
-                                <button type='submit'>Login</button>
+                                <button type='submit' className='shadow'>Login</button>
                             </div>
+                            <button className='btn btn-light-primary px-5 py-3 rounded-5 w-100 mt-2'>
+                                <div className='d-flex gap-4 gap-md-5 align-items-center '>
+                                    <span className='d-flex align-items-center justify-content-center p-2 btn-light text-light-primary rounded-circle fs-2' style={{ width: 35, height: 35 }}>
+                                        <i class="fab fa-facebook-f"></i>
+                                    </span>
+                                    <span className=' fs-2 text-light'>Continue with Google</span>
+                                </div>
+                            </button>
                         </div>
                     </div>
 
